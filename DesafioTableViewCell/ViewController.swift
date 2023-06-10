c0283980551b72654b3c22c52e4bdc58cea13aed
Gustavo Silva <premiersoft-gustavo.cabral@ailos.coop.br>
2023-06-10 14:25:28 -0300

    Initial Commit

---
+++ DesafioTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioTableViewCell
+//
+//  Created by Premiersoft on 10/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
